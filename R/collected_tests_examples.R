f3afc72af7da22c31bb714590201c40b2cf2d8b0
AntonioJBT <a.berlanga@imperial.ac.uk>
2018-10-03 18:40:44 +0100

    moving functions to individual files ...

--- R/collected_tests_examples.R
+++ R/collected_tests_examples.R
@@ -16,3 +16,12 @@
 #
 # #
 # epi_list_tail(as.list(df), 5, 4)
+
+# # Test:
+# dim(df)
+# epi_head_and_tail(df, rows = 2, cols = 2)
+# # Get all duplicates:
+# check_dups <- epi_get_all_dups(df, 'var_id', 1)
+# dim(check_dups)
+# check_dups
+
---
+++ R/epi_get_all_dups.R
@@ -0,0 +1,51 @@
+#' @title Get all duplicated rows including the originals
+#'
+#' epi_get_all_dups() Get all elements which are repeated,
+#' including the originals.
+#'
+#' @description base R duplicated() does not return the originals (duplicated - 1)
+#' See for example https://stackoverflow.com/questions/16905425/find-duplicate-values-in-r
+#'
+#' @param df A data.frame to extract duplicates from
+#'
+#' @param var Variable/column name as a string to use to detect duplicate values
+#'
+#' @param freq Frequency count, an integer. Defaults to 1 (so will return original
+#' plus any repeating values)
+#'
+#' @note 	Creates a table with frequencies, checks those which have more than 1
+#' (or value passed to freq and considers these as variables with duplicates)
+#' and extracts them.
+#'
+#' @return a data.frame with original and duplicated rows
+#'
+#' @author Antonio Berlanga-Taylor <\url{https://github.com/AntonioJBT/episcout}>
+#'
+#' @seealso \code{\link{duplicated}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' dim(df)
+#' epi_head_and_tail(df, rows = 2, cols = 2)
+#' # Get all duplicates:
+#' check_dups <- epi_get_all_dups(df, 'var_id', 1)
+#' dim(check_dups)
+#' check_dups
+#' }
+#'
+#' @export
+#'
+
+epi_get_all_dups <- function(df = NULL,
+														 var = '',
+														 freq = 1
+														 ) {
+	# Create a table with frequencies:
+	n_occur <- data.frame(table(df[[var]]))
+	# Check those which have mor than 1, these are duplicated:
+	dups <- n_occur$Freq > freq
+	# Extract them:
+	df_dups <- df[df[[var]] %in% n_occur$Var1[dups], ]
+	return(df_dups)
+}
