e30c869e7059e2523eaab749b560d73911c6d08a
AntonioJBT <a.berlanga@imperial.ac.uk>
2018-11-02 17:21:32 +0000

    adding tests, funcs under various have tests, started epi_clean

---
+++ tests/testthat/test-cleaning_functions.R
@@ -0,0 +1,55 @@
+context("episcout cleaning and utility")
+
+######################
+library(episcout)
+library(testthat)
+######################
+
+# Working directory for informal tests, should be from pkg/tests/testthat/:
+# setwd("")
+
+######################
+# Get all duplicates:
+print("Function being tested: epi_clean_get_dups")
+
+test_that("Test expected output after epi_clean_get_dups", {
+	# Should contain all the df (because each row is duplicated
+	# if looking at 'var_id' only)
+	check_dups <- epi_clean_get_dups(df, 'var_id', 1)
+	# str(dim(check_dups))
+	# check_dups
+	expect_output(str(dim(check_dups)), ' 20 5')
+	expect_output(str(head(check_dups)), 'var_id    : int  1 1 2 2 3 3')
+	# Should be empty:
+	check_dups <- epi_clean_get_dups(df, 'var_id', 2)
+	# str(dim(check_dups))
+	# str(check_dups)
+	expect_output(str(dim(check_dups)), ' 0 5')
+	expect_output(str(check_dups), '0 obs. of  5 variables:')
+}
+)
+######################
+
+######################
+# TO DO: continue here
+print("Function being tested: epi_XX_XX")
+
+test_that("Test expected output after epi_XX_XX", {
+  # output is silent if successful
+  # matches values, attributes, and type:
+  expect_identical(func_test, expected_result)
+  # matches values and attributes, adjust with tolerance parameter:
+  expect_equal(func_test, expected_result)
+  expect_equal(2 * 2, 4)
+  # matches values only:
+  expect_equivalent(funct_test, verbatim_screen_output)
+  # match (partial) string output:
+  expect_output(str(dim(an_object), expected_result_as_string))
+  # match a more complex output stored in a file:
+  expect_output_file(str(airquality), "airq.txt", update = TRUE) # create a file
+  expect_output_file(str(airquality), "airq.txt") # compare the contents
+  # test non-Exported functions (triple colon):
+  expect_equal(my_pkg:::func_not_exported(airquality$Ozone), 37)
+  }
+  )
+######################
---
+++ tests/testthat/test-various_not_classed.R
@@ -0,0 +1,99 @@
+######################
+context("episcout test functions not classed, ie not in clean, plot, etc.")
+######################
+
+######################
+library(episcout)
+library(testthat)
+######################
+
+# Working directory for informal tests, should be from pkg/tests/testthat/:
+# setwd('/Users/antoniob/Documents/github.dir/AntonioJBT/episcout/tests/testthat/')
+
+######################
+# Test set df:
+set.seed(12345)
+n <- 20
+df <- data.frame(
+	var_id = rep(1:(n / 2), each = 2),
+  var_to_rep = rep(c('Pre', 'Post'), n / 2),
+	x = rnorm(n),
+	y = rbinom(n, 1, 0.50),
+	z = rpois(n, 2)
+	)
+# df
+
+# And save to disk with:
+# epi_write(df, 'inst/extdata/df.tsv')
+# df.tsv is then used for tests below
+######################
+
+######################
+print('Function being tested: epi_read')
+
+test_that("Test expected output after epi_read", {
+	input_file <- '../../inst/extdata/df.tsv'
+	test_df <- epi_read(input_file) # relative to tests/testthat/
+	# str(dim(test_df))
+	# str(head(test_df, 1))
+	expect_output(str(dim(test_df)), '20 5')
+	expect_output(str(dim(mtcars)), '32 11')
+	expect_output(str(head(test_df, 1)), 'var_id    : int 1')
+	expect_output(str(head(test_df, 1)), 'var_to_rep: chr "Pre"')
+	expect_output(str(head(test_df, 1)), 'x         : num 0.586')
+	expect_output(str(tail(test_df, 1)), 'x         : num 0.299')
+	expect_output(str(tail(test_df, 1)), 'var_to_rep: chr "Post"')
+	}
+)
+######################
+
+######################
+print("Function being tested: epi_write")
+
+test_that("Test expected output after epi_write", {
+	# epi_write(df, '') # write to stdout with data.table
+	expect_output(epi_write(df, ''), 'var_id	var_to_rep	x	y	z')
+	expect_output(epi_write(df, ''), '0.585528817843856')
+	expect_output(epi_write(df, ''), '10	Post	0.298723699267293	0	1')
+	}
+)
+######################
+
+######################
+print("Function being tested: epi_head_and_tail")
+
+test_that("Test expected output after epi_head_and_tail", {
+	# epi_head_and_tail(df, rows = 2, cols = 2)
+	# epi_head_and_tail(df, rows = 2, cols = 2, last_cols = TRUE)
+	expect_output(epi_head_and_tail(df, rows = 2, cols = 2), '2       1       Post')
+	expect_output(epi_head_and_tail(df, rows = 2, cols = 2), '20     10       Post')
+	expect_output(epi_head_and_tail(df, rows = 2, cols = 2, last_cols = TRUE), '1   0.5855288 1 3')
+	expect_output(epi_head_and_tail(df, rows = 2, cols = 2, last_cols = TRUE), '20  0.2987237 0 1')
+	}
+	)
+######################
+
+######################
+print("Function being tested: epi_list_head and epi_list_tail")
+
+test_that("Test expected output after epi_list_head and epi_list_tail", {
+	# epi_list_head(as.list(df), 5, 4)
+	# epi_list_tail(as.list(df), 5, 4)
+	expect_output(epi_list_head(as.list(df), 5, 4), 'List has 5 elements in total.')
+	expect_output(epi_list_head(as.list(df), 5, 4), 'Levels: Post Pre')
+	expect_output(epi_list_tail(as.list(df), 5, 4), 'Last 5 rows of first 4 elements in list: ')
+	expect_output(epi_list_tail(as.list(df), 5, 4), '0 1 0 0 0')
+	}
+)
+######################
+
+######################
+print("Function being tested: epi_output_name")
+
+test_that("Test expected output after epi_output_name", {
+	str_output <- epi_output_name(input_name = input_file, suffix = '.last_dot_only')
+	# str(str_output)
+	expect_output(str(str_output), '../../inst/extdata/df.last_dot_only')
+	}
+)
+######################
