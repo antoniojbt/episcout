a01b7ec78fa3b1ee41ca917ea15e2419d5821d85
AntonioJBT <a.berlanga@imperial.ac.uk>
2018-11-06 12:22:10 +0000

    more tests and docs for cleaning/pre-processing functions

--- R/epi_clean.R
+++ R/epi_clean.R
@@ -1,6 +1,6 @@
-######################
-# Compare two columns which may have duplicated information
-# TO DO: test and complete, not working
+#####################
+#' Compare two columns which may have duplicated information
+#' TO DO: test and complete, not working
 # epi_clean_compare_dup_cols <- function(df, col_1, col_2) {
 # 	require(compare)
 # 	df[[col_1]] <- enc2utf8(df[[col_1]])
@@ -15,59 +15,14 @@
 # 											 )
 # 	return(comp_results)
 # }
-# Test:
-######################
-
-######################
-# Convert any chr class to factor if it has less than x unique results
-# Avoid converting dates to factors though, only works if already specified as date
-# it class is chr and number of unique values is less than the cut-off passed it'll
-# get converted to factor
-# Note that lubridate converted dates may return eg
-# "POSIXct" "POSIXt"
-# which with
-# class(df[[i]]) != 'Date'
-# will return a logical vector > 1 (ie
-# [1] TRUE TRUE
-# which will give a warning message:
-# "... the condition has length > 1 and only the first element will be used"
-epi_clean_class_to_factor <- function(df, cutoff_unique = 10){
-	for (i in seq_along(df)) {
-		if (
-			# if num. of unique values is less than cut-off
-			length(unique(df[[i]])) < cutoff_unique &
-			# and the class is not already a date:
-			class(df[[i]]) != 'Date'
-		) {
-			# Convert to factor:
-			df[[i]] <- as.factor(df[[i]])
-		}
-	}
-	return(df)
-}
-# # Test:
-# df_factor <- df
-# df_factor$date_col <- seq(as.Date("2018/1/1"), by = "year", length.out = 5)#nrow(df_factor))
-# lapply(df_factor, class)
-# lapply(df_factor, function(x) length(unique(x)))
-# # Check conditions:
-# i <- 'date_col'
-# cutoff_unique <- 10
-# # if num. of unique values is less than cut-off:
-# length(unique(df_factor[[i]])) < cutoff_unique # should be TRUE
-# # and the class is not already a date:
-# class(df_factor[[i]]) != "Date" # should be FALSE
-# epi_clean_class_to_factor(df_factor) # should return date_col as Date
-# df_factor <- epi_clean_class_to_factor(df_factor, cutoff_unique = 10)
-# lapply(df_factor, class)
-# df_factor
-######################
+#' Test:
+#####################
 
 #####################
-# Replace values with string in a column
-# Uses stringr to match pattern provided and replace value in column with
-# string provided
-# Useful for partial matching for eg dates and replacing with NA
+#' Replace values with string in a column
+#' Uses stringr to match pattern provided and replace value in column with
+#' string provided
+#' Useful for partial matching for eg dates and replacing with NA
 epi_clean_replace_value <- function(df, col_id, pattern, replace_str = NA) {
 	if (!requireNamespace('stringr', quietly = TRUE)) {
 		stop("Package stringr needed for this function to work. Please install it.",
@@ -80,32 +35,32 @@ epi_clean_replace_value <- function(df, col_id, pattern, replace_str = NA) {
 	# df[[col_id]][which(str_detect(df[[col_id]], pattern = patterns))] <- NA
 	return(df[[col_id]])
 }
-# # Test:
-# df_factor <- df
-# df_factor$date_col <- seq(as.Date("2018/1/1"), by = "year", length.out = 5)
-# # Convert to character first:
-# df_factor$date_col <- as.character(df_factor$date_col)
-# lapply(df_factor, class)
-# patterns <- c('2018', '2022')
-# pattern <- pattern <- sprintf('^%s', patterns[1]) # match values starting with string
-# epi_clean_replace_value(df_factor, 'date_col', pattern, NA)
-# df_factor$date_col
-# # In a loop:
-# for (i in seq_along(df_factor)) {
-# 	for (p in patterns) {
-# 		# match values starting with string:
-# 		pattern <- sprintf('^%s', p)
-# 		df_factor[[i]] <- epi_clean_replace_value(df_factor, i, pattern, NA)
-# 	}
-# }
-# df_factor$date_col
+#' # Test:
+#' df_factor <- df
+#' df_factor$date_col <- seq(as.Date("2018/1/1"), by = "year", length.out = 5)
+#' # Convert to character first:
+#' df_factor$date_col <- as.character(df_factor$date_col)
+#' lapply(df_factor, class)
+#' patterns <- c('2018', '2022')
+#' pattern <- pattern <- sprintf('^%s', patterns[1]) #' match values starting with string
+#' epi_clean_replace_value(df_factor, 'date_col', pattern, NA)
+#' df_factor$date_col
+#' # In a loop:
+#' for (i in seq_along(df_factor)) {
+#' 	for (p in patterns) {
+#' 		#' match values starting with string:
+#' 		pattern <- sprintf('^%s', p)
+#' 		df_factor[[i]] <- epi_clean_replace_value(df_factor, i, pattern, NA)
+#' 	}
+#' }
+#' df_factor$date_col
 #####################
 
 ######################
-# Add a column with count of duplicate (repeated screening, replicate count)
-# Assumes df is sorted by id and eg if date is used, then earlier dates are first
-# Returns a dataframe with one column which can be merged with existing df
-# Useful when there are more than 2 replicates for each row.
+#' Add a column with count of duplicate (repeated screening, replicate count)
+#' Assumes df is sorted by id and eg if date is used, then earlier dates are first
+#' Returns a dataframe with one column which can be merged with existing df
+#' Useful when there are more than 2 replicates for each row.
 epi_clean_add_rep_num <- function(df, var_id, var_to_rep) {
 	output <- data.frame(var_id = df[[var_id]],
 											 rep_num = rep(1, nrow(df)) # create a vector of 1's
@@ -129,49 +84,49 @@ epi_clean_add_rep_num <- function(df, var_id, var_to_rep) {
 	}
   return(output)
 }
-# Test variables:
-# var_id <- 'var_id'
-# var_to_rep <- 'var_to_rep'
-# df
-# res <- epi_clean_add_rep_num(df, 'var_id', 'var_to_rep')
-# res
-# # Sanity check:
-# identical(as.character(res[[var_id]]),
-# 					as.character(df[[var_id]])) # should be TRUE
-# # Bind:
-# df2 <- as.tibble(cbind(df, 'rep_num' = res$rep_num))
-# # merge() will adds all rows from both data frames as there are duplicates
-# # so use cbind after making sure order is exact
-# epi_head_and_tail(df2, rows = 3)
-# epi_head_and_tail(df2, rows = 3, last_cols = TRUE)
-# df2
+#' Test variables:
+#' var_id <- 'var_id'
+#' var_to_rep <- 'var_to_rep'
+#' df
+#' res <- epi_clean_add_rep_num(df, 'var_id', 'var_to_rep')
+#' res
+#' # Sanity check:
+#' identical(as.character(res[[var_id]]),
+#' 					as.character(df[[var_id]])) # should be TRUE
+#' # Bind:
+#' df2 <- as.tibble(cbind(df, 'rep_num' = res$rep_num))
+#' # merge() will adds all rows from both data frames as there are duplicates
+#' #' so use cbind after making sure order is exact
+#' epi_head_and_tail(df2, rows = 3)
+#' epi_head_and_tail(df2, rows = 3, last_cols = TRUE)
+#' df2
 ######################
 
 ######################
-# Change col names to baseline, time_1, time_2, etc.:
+#' Change col names to baseline, time_1, time_2, etc.:
 epi_clean_add_colname_suffix <- function(df, id_col_num, suffix) {
 	col_names <- names(df)[-id_col_num]
 	col_names <- paste(col_names, suffix, sep = '')
 	# names(df)[start_at:ncol(df)] <- col_names
 	return(col_names)
 }
-# # Test
-# df2 <- df
-# names(df2)
-# # Add .0 as suffix to all column names starting from column 2 (skip id col):
-# id_col <- 1
-# new_colnames <- epi_clean_add_colname_suffix(df2, id_col, '.0')
-# new_colnames
-# # Rename them in my data frame:
-# names(df2)[-id_col] <- new_colnames
-# names(df2)
+#' # Test
+#' df2 <- df
+#' names(df2)
+#' #' Add .0 as suffix to all column names starting from column 2 (skip id col):
+#' id_col <- 1
+#' new_colnames <- epi_clean_add_colname_suffix(df2, id_col, '.0')
+#' new_colnames
+#' #' Rename them in my data frame:
+#' names(df2)[-id_col] <- new_colnames
+#' names(df2)
 ######################
 
 ######################
-# Create a single data frame with unique rows (individuals) and repeated
-# observations across columns
-# A column with the replicate/repeated observation/time-point number for each row
-# must be provided
+#' Create a single data frame with unique rows (individuals) and repeated
+#' observations across columns
+#' A column with the replicate/repeated observation/time-point number for each row
+#' must be provided
 epi_clean_spread_repeated <- function(df, rep_num_col, id_col_num) {
 	reps <- unique(df[[rep_num_col]])
 	output <- vector(mode = 'list')#, length = length(reps))
@@ -188,22 +143,22 @@ epi_clean_spread_repeated <- function(df, rep_num_col, id_col_num) {
 	}
 	return(output)
 }
-# Test:
-# df3 <- df
-# df3
-# df3 <- epi_clean_spread_repeated(df3, 'var_to_rep', 1)
-# df3
+#' Test:
+#' df3 <- df
+#' df3
+#' df3 <- epi_clean_spread_repeated(df3, 'var_to_rep', 1)
+#' df3
 ######################
 
 ######################
-# Recursively merge data frames that are stored as lists within a list
-# Flatten with eg purrr::flatten() if there is more than one level.
-# Assumes:
-# - there are are 3 or more data frames to merge
-# - there are no duplicates in any of the data frames
-# The function performs a full outer join with base R
-# merge(df1, df2, by = id_col, all = TRUE)
-# TO DO: if only two dfs in list, print message or do one merge
+#' Recursively merge data frames that are stored as lists within a list
+#' Flatten with eg purrr::flatten() if there is more than one level.
+#' Assumes:
+#' - there are are 3 or more data frames to merge
+#' - there are no duplicates in any of the data frames
+#' The function performs a full outer join with base R
+#' merge(df1, df2, by = id_col, all = TRUE)
+#' TO DO: if only two dfs in list, print message or do one merge
 epi_clean_merge_nested_dfs <- function(nested_list_dfs, id_col) {
 	# Initialise merge:
 	df1 <- nested_list_dfs[[1]]
@@ -218,66 +173,66 @@ epi_clean_merge_nested_dfs <- function(nested_list_dfs, id_col) {
 		}
   return(temp_df)
 	}
-# # Test
-# library(purrr)
-# library(tibble)
-# nested_list_dfs <- purrr::flatten(list(df3, df3, df3))
-# id_col <- 'var_id'
-# epi_list_head(nested_list_dfs, 2, 3)
-# epi_list_tail(nested_list_dfs, 2, 3)
-# all_merged <- epi_clean_merge_nested_dfs(nested_list_dfs, id_col)
-# dim(all_merged)
-# as.tibble(all_merged)
-# names(all_merged)
+#' # Test
+#' library(purrr)
+#' library(tibble)
+#' nested_list_dfs <- purrr::flatten(list(df3, df3, df3))
+#' id_col <- 'var_id'
+#' epi_list_head(nested_list_dfs, 2, 3)
+#' epi_list_tail(nested_list_dfs, 2, 3)
+#' all_merged <- epi_clean_merge_nested_dfs(nested_list_dfs, id_col)
+#' dim(all_merged)
+#' as.tibble(all_merged)
+#' names(all_merged)
+#'
+#' # This function would be equivalent to iteratively doing:
+#' # Create sets with distinct observations:
+#' library(dplyr)
+#' # use df2 after custom function above add_rep_num():
+#' df2
+#' # See how many replicates there are:
+#' df2 %>%
+#' 	transmute(as.factor(rep_num)) %>%
+#' 	summary()
+#'
+#' # Generate a data frame for each:
+#' baseline <- df2 %>%
+#' 	filter(rep_num == 1)
+#' baseline
+#' # Sanity check, should be empty:
+#' epi_clean_get_dups(baseline, 'var_id', 1)
+#' # Change col names to baseline, time_1, time_2, etc.:
+#' new_colnames <- epi_clean_add_colname_suffix(baseline, 1, '.0')
+#' names(baseline)[2:ncol(baseline)] <- new_colnames
+#' names(baseline)
 #
-# # This function would be equivalent to iteratively doing:
-# # Create sets with distinct observations:
-# library(dplyr)
-# # use df2 after custom function above add_rep_num():
-# df2
-# # See how many replicates there are:
-# df2 %>%
-# 	transmute(as.factor(rep_num)) %>%
-# 	summary()
+#' # First set of repeated observations:
+#' time_1 <- df2 %>%
+#' 	filter(rep_num == 2)
+#' time_1
+#' epi_clean_get_dups(time_1, 'var_id', 1)
+#' # Change col names:
+#' new_colnames <- epi_clean_add_colname_suffix(time_1, 1, '.1')
+#' names(time_1)[2:ncol(time_1)] <- new_colnames
+#' names(time_1)
 #
-# # Generate a data frame for each:
-# baseline <- df2 %>%
-# 	filter(rep_num == 1)
-# baseline
-# # Sanity check, should be empty:
-# epi_clean_get_dups(baseline, 'var_id', 1)
-# # Change col names to baseline, time_1, time_2, etc.:
-# new_colnames <- epi_clean_add_colname_suffix(baseline, 1, '.0')
-# names(baseline)[2:ncol(baseline)] <- new_colnames
-# names(baseline)
+#' # Nothing left:
+#' df2 %>%
+#' 	filter(rep_num == 3)
 #
-# # First set of repeated observations:
-# time_1 <- df2 %>%
-# 	filter(rep_num == 2)
-# time_1
-# epi_clean_get_dups(time_1, 'var_id', 1)
-# # Change col names:
-# new_colnames <- epi_clean_add_colname_suffix(time_1, 1, '.1')
-# names(time_1)[2:ncol(time_1)] <- new_colnames
-# names(time_1)
-#
-# # Nothing left:
-# df2 %>%
-# 	filter(rep_num == 3)
-#
-# # Merge the data frames into one:
-# all_merged <- merge(baseline, time_1, by = 'var_id', all = TRUE)
-# dim(all_merged)
-# as.tibble(all_merged)
-# names(all_merged)
-# epi_head_and_tail(all_merged)
-# epi_head_and_tail(all_merged, last_cols = TRUE)
-# View(all_merged)
+#' # Merge the data frames into one:
+#' all_merged <- merge(baseline, time_1, by = 'var_id', all = TRUE)
+#' dim(all_merged)
+#' as.tibble(all_merged)
+#' names(all_merged)
+#' epi_head_and_tail(all_merged)
+#' epi_head_and_tail(all_merged, last_cols = TRUE)
+#' View(all_merged)
 ######################
 
 ######################
-# Transposes a data frame file and preserves row and column names:
-# Assumes there is an id column with unique IDs
+#' Transposes a data frame file and preserves row and column names:
+#' Assumes there is an id column with unique IDs
 epi_clean_transpose <- function(df, id_col_num) {
 	if (!requireNamespace('data.table', quietly = TRUE)) {
 		stop("Package data.table needed for this function to work. Please install it.",
@@ -295,15 +250,15 @@ epi_clean_transpose <- function(df, id_col_num) {
 	df_t <- cbind(as.character(cols[-1]), df_t) # Exclude first label
 	return(as.data.frame(df_t))
 	}
-# Test:
-# df_trans <- df
-# df_trans$id_col <- rownames(df_trans)
-# df_trans
-# id_col <- 6
-# df_t <- epi_clean_transpose(df = df_trans, id_col)
-# class(df_t)
-# dim(df)
-# dim(df_t)
-# df_t
-# names(df_t)
+#' Test:
+#' df_trans <- df
+#' df_trans$id_col <- rownames(df_trans)
+#' df_trans
+#' id_col <- 6
+#' df_t <- epi_clean_transpose(df = df_trans, id_col)
+#' class(df_t)
+#' dim(df)
+#' dim(df_t)
+#' df_t
+#' names(df_t)
 ######################
