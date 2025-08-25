26854074886a06d6a1df393d26e631e6039d6a1b
antoniojbt <5346783+antoniojbt@users.noreply.github.com>
2025-08-25 12:05:08 -0600

    [Fix] Add tests for epi_clean_int_to_factor?

---
+++ tests/testthat/test-clean-int-to-factor.R
@@ -0,0 +1,13 @@
+context("epi_clean_int_to_factor")
+
+test_that("integer columns are converted to factors", {
+  dt <- data.table::data.table(
+    a = 1:3,
+    b = as.integer(4:6),
+    c = data.table::as.IDate("2020-01-01") + 0:2
+  )
+  res <- epi_clean_int_to_factor(dt)
+  expect_true(is.factor(res$a))
+  expect_true(is.factor(res$b))
+  expect_true(inherits(res$c, "IDate"))
+})
